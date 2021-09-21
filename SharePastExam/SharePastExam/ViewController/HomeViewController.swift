4e555d3f613e9d97c997a58ccd270bd6cdc578c3
japonica0k <ima1.tosaka89@ezweb.ne.jp>
2021-09-21 17:02:35 +0900

    <create> Foundation of window

---
+++ SharePastExam/SharePastExam/ViewController/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  SharePastExam
+//
+//  Created by Takashi Sakai on 2021/09/21.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
