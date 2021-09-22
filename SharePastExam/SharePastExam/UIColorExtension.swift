edd37a6b305b65f353691a56ac0090a1547eb4ee
japonica0k <ima1.tosaka89@ezweb.ne.jp>
2021-09-22 14:47:56 +0900

    <Joint> LoginFirebaseのコミット

---
+++ SharePastExam/SharePastExam/UIColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColorExtension.swift
+//  SharePastExam
+//
+//  Created by 馬場大夢 on 2021/09/17.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return self.init(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1)
+    }
+}
