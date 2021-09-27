33670acba773014e16469461940d42f2b0498c95
japonica0k <ima1.tosaka89@ezweb.ne.jp>
2021-09-27 17:17:13 +0900

    <create> main screen

---
+++ SharePastExam/SharePastExam/viewControllers/SelectionSubjectionController 2.swift
@@ -0,0 +1,16 @@
+//
+//  SelectionSubjectionController.swift
+//  SharePastExam
+//
+//  Created by Takashi Sakai on 2021/09/27.
+//
+
+import UIKit
+import Foundation
+
+class SelectionSubjectionController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
