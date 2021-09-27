0f57fde60653fc8d3b684b5f91496acf2c58c943
japonica0k <ima1.tosaka89@ezweb.ne.jp>
2021-09-27 18:26:59 +0900

    <create>HomeViewController <change>mainstoryboard

---
+++ SharePastExam/SharePastExam/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  SharePastExam
+//
+//  Created by Takashi Sakai on 2021/09/27.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
