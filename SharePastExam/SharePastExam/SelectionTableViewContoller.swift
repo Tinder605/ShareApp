c5439ce0cbbf67bd69b672e95042cde3454ca680
japonica0k <ima1.tosaka89@ezweb.ne.jp>
2021-09-27 19:07:41 +0900

    <change> tableviewの付け加え

---
+++ SharePastExam/SharePastExam/SelectionTableViewContoller.swift
@@ -0,0 +1,16 @@
+//
+//  SelectionTableViewContoller.swift
+//  SharePastExam
+//
+//  Created by Takashi Sakai on 2021/09/27.
+//
+
+import Foundation
+import UIKit
+
+class SelectionTableViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
