74e2869f811857b57c9186eaf309f3c94dc4603b
japonica0k <ima1.tosaka89@ezweb.ne.jp>
2021-11-05 15:28:11 +0900

    <create> xibfile

--- SharePastExam/SharePastExam/ShareRoomCollectionCellView.swift
+++ SharePastExam/SharePastExam/ShareRoomCollectionCellView.swift
@@ -1,25 +1,46 @@
-////
-////  ShareRoomCollectionCellView.swift
-////  SharePastExam
-////
-////  Created by Takashi Sakai on 2021/10/22.
-////
 //
-//import Foundation
-//import UIKit
+//  ShareRoomCollectionCellView.swift
+//  SharePastExam
 //
-//class ShareRoomCollectionViewCell : UICollectionViewCell {
-//    static let identifier = ""
+//  Created by Takashi Sakai on 2021/10/22.
 //
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
+
+import Foundation
+import UIKit
+
+class ShareRoomCollectionCellView : UICollectionViewCell {
+    static let identifier = "cellid"
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+//        self.fittoview(wid: (width-30)/3, high: (width-30)/3)
+        
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+ 
+    static func nib() -> UINib {
+        return UINib(nibName: "goodCollectionViewCell", bundle: nil)
+    }
+//    static func makelabel(text:String) {
+//        let view = UINib(nibName: "ShareRoomCollectionViewCell", bundle: nil).instantiate(withOwner: nil, options: nil).first as! ShareRoomCollectionCellView
+//
+//        view.TextTitleLabel.text = "aks"
+//
 //    }
-//
-//    public func configure(with image: UIImage) {
-//        imageView.image = image
+    
+//    public func configure(title :String){
+//        TextTitleLabel.text = title
 //    }
+    
+    
 //
 //    static func nib() -> UINib {
-//        return UINib(nibName: "MyCollectionViewCell", bundle: nil)
+//        return UINib(nibName: "ShareRoomCollectionViewCell", bundle: nil)
 //    }
-//}
+}
--- SharePastExam/SharePastExam/ShareRoomViewController.swift
+++ SharePastExam/SharePastExam/ShareRoomViewController.swift
@@ -10,8 +10,8 @@ import SDWebImage
 import Firebase
 import FirebaseStorage
 
-class ShareRoomViewController: UIViewController {
-    
+class ShareRoomViewController: UIViewController, UICollectionViewDataSource ,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
+
     @IBOutlet weak var PostButton: UIButton!
     @IBOutlet weak var ShareRoomCollectionView: UICollectionView!
     @IBOutlet weak var ShareRoomViewHeight: NSLayoutConstraint!
@@ -45,13 +45,44 @@ class ShareRoomViewController: UIViewController {
         navigationItem.title = timestile
         UserDefaults.standard.set(timestile, forKey: "RecentlyTimes")
         
+        let nib = UINib(nibName: "ShareRoomCollectionViewCell", bundle: nil)
+        self.ShareRoomCollectionView.register(nib, forCellWithReuseIdentifier: "CellView")
+        ShareRoomCollectionView.dataSource = self
+        ShareRoomCollectionView.delegate = self
+        ShareRoomCollectionView.isScrollEnabled = true
         
-        //ShareRoomCollectionView.delegate = self
-        //ShareRoomCollectionView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellWithReuseIdentifier: <#T##String#>)
+        
+        
+//        let lay = ShareRoomCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//        lay.itemSize = CGSize(width: (width-10)/3, height: 100)
+//        print(lay.itemSize)
+        
+//        let layout = UICollectionViewFlowLayout()
+//        layout.estimatedItemSize = .zero
+//        layout.itemSize = CGSize(width: (width-10)/3, height: 120)
+//        ShareRoomCollectionView.collectionViewLayout = layout
+    }
+
+}
+
+extension ShareRoomViewController {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.view.frame.width
+        let wid = (width-30)/3
+        return .init(width: wid, height: wid)
     }
     
     
-    
-    
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellView", for: indexPath) as! ShareRoomCollectionCellView
+        //cell.fittoview(width: (width-30)/3, height: (width-30)/3)
+        return cell
+    }
+
 }
 
